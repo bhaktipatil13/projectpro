f7a25cb4b1cc1e6b306198564c9441e37f40f5da
bhaktipatil13 <67259038+bhaktipatil13@users.noreply.github.com>
2020-06-27 20:18:54 +0530

    Update Projects.js

--- src/constants/Projects.js
+++ src/constants/Projects.js
@@ -14,30 +14,6 @@ export const projects = {
             name: "Project 3",
             //description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
             //link: "http://madhavbahl.tech/react-pokedex/"
-        }, {
-            name: "Project 4",
-           // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            //link: "http://madhavbahl.tech/react-pokedex/"
-        }, {
-            name: "Project 5",
-            //description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-           // link: "http://madhavbahl.tech/react-pokedex/"
-        }, {
-            name: "Project 6",
-            //description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            //link: "http://madhavbahl.tech/react-pokedex/"
-        }, {
-            name: "Project 7",
-            //description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            //link: "http://madhavbahl.tech/react-pokedex/"
-        }, {
-            name: "Project 8",
-           // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-           // link: "http://madhavbahl.tech/react-pokedex/"
-        }, {
-            name: "Project 9",
-            //description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-           // link: "http://madhavbahl.tech/react-pokedex/"
-        }
+        } 
     ]
 }
